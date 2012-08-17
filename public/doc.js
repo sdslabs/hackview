6080079cf391e5394f114833ed0abb276fb75678
Abhay Rana <capt.n3m0@gmail.com>
2012-08-18 03:08:06 +0530

    Condense code for calling JSONP

--- public/doc.js
+++ public/doc.js
@@ -1,15 +1,15 @@
 var Doc=(function(){
 	//attach to events here
+
+	//flickr helper function to call flickr API via YQL
+	//returns results to the callback function
 	var _flickr = function(query,callback){
-		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.search%20where%20text%3D%22' + query + '%22%20and%20api_key%3D%2241386f9cfb34101b940afa34f6bfba2f%22%20limit%2010&diagnostics=true';
-		$.ajax({
-			url: url,
-			dataType: 'jsonp'
-		}).done(callback);
-	}
+		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.search%20where%20text%3D%22' + escape(query) + '%22%20and%20api_key%3D%2241386f9cfb34101b940afa34f6bfba2f%22%20limit%2010&diagnostics=true&callback=?';
+		$.getJSON(url,callback);
+	};
 
 	return {
 	  render: null,
 	  flickr: _flickr
 	};
-})()
+})();
