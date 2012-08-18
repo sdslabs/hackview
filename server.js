c439850f255f1a108fabb23c0d280ba1a37afec2
Abhay Rana <capt.n3m0@gmail.com>
2012-08-19 02:04:08 +0530

    use port from heroku env

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var options = {db: {type: 'none'}};
 sharejs.server.attach(app, options);
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
-app.listen(8000);
-console.log('App running on port 8000');
+
+var port = process.env.PORT || 8000;
+
+app.listen(port);
+console.log('App running on port'+port);
 var webRTC = require('webrtc.io').listen(app);
 
 require('./rtc.js')(webRTC);
