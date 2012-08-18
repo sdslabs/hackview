9476971400426b50191fb708c757733ad90c347e
Abhay Rana <capt.n3m0@gmail.com>
2012-08-18 06:46:54 +0530

    basic text editor working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express=require('express'),
 var options = {db: {type: 'none'}};
 
 sharejs.server.attach(app, options);
-
+app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 app.listen(8000);
 console.log('App running on port 8000');
