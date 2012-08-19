09b77630658915e2e5e03111c62f8e842aa50ee3
Abhay Rana <capt.n3m0@gmail.com>
2012-08-19 05:59:57 +0530

    Introduced redis for shareJS
    - Only works if it finds process.env.redis_port
    - The config is available in cloudno.de
    - Redis will store the database in storage

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 var express=require('express'),
     app = express.createServer(),
-    sharejs = require('share');
+    sharejs = require('share'),
+    sharejsOptions={db:{type:'none'}};
 
-var options = {db: {type: 'none'}};
+var env = process.env.NODE_ENV;
 
-sharejs.server.attach(app, options);
-app.use(express.logger('dev'));
+//if we were provided redis options, use them for persistence
+//the if case is for developers not using redis
+if(process.env.redis_port){
+	sharejsOptions.db= {
+		type: 'redis',
+		prefix: '',
+		port: process.env.redis_port,
+		auth: process.env.redis_auth || null
+	}
+};
+
+sharejs.server.attach(app, sharejsOptions);//attach to express
+
+
+
+if (env !== 'production')
+	app.use(express.logger('dev'));
+
 app.use(express.static(__dirname + '/public'));
 
+//heroku support
 var port = process.env.PORT || 8000;
-
 app.listen(port);
 console.log('App running on port'+port);
-var webRTC = require('webrtc.io').listen(app);
 
+//webRTC Stuff
+var webRTC = require('webrtc.io').listen(app);
 require('./rtc.js')(webRTC);
+
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
